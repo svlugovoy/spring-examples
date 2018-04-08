b0fa3ee491b1d27f838d229c220f28c0d531f3ad
svlugovoy <svlugovoy@gmail.com>
2018-04-08 18:42:48 +0300

    Added example with method replacement

---
+++ 04-spring-method-replacement/src/main/java/com/svlugovoy/methodreplacement/ReplacementTarget.java
@@ -0,0 +1,12 @@
+package com.svlugovoy.methodreplacement;
+
+public class ReplacementTarget {
+
+    public String formatMessage(String msg) {
+        return "<h1>" + msg + "</h1>"; 
+    }
+
+    public String formatMessage(Object msg) {
+        return "<h1>" + msg + "</h1>"; 
+    }
+}
